4dca0e6d63d31021a0db647758cea1ae2c90e84d
Fernando Corrales <corrales_fernando@hotmail.com>
2024-12-01 18:16:48 -0300

    add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
+import cookieParser from 'cookie-parser'
 import express from 'express'
 import morgan from 'morgan'
 import mainRouter from './routes/main'
-import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -10,6 +10,14 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 app.use(cookieParser())
+
+// Enable CORS for all routes
+app.use((_, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 app.use('/api', mainRouter)
 
